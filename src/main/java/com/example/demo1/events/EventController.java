4f2b119e8449f35039c112cf69e09c5138b0d6a8
saama <hjpw237@naver.com>
2021-08-13 04:17:57 +0900

    validate wrong data

--- src/main/java/com/example/demo1/events/EventController.java
+++ src/main/java/com/example/demo1/events/EventController.java
@@ -20,10 +20,12 @@ public class EventController {
 
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
+    private final EventValidator eventValidator;
 
-    public EventController( EventRepository eventRepository, ModelMapper modelMapper){
+    public EventController( EventRepository eventRepository, ModelMapper modelMapper, EventValidator eventValidator){
         this.eventRepository = eventRepository;
         this.modelMapper = modelMapper;
+        this.eventValidator = eventValidator;
     }
 
     @PostMapping
@@ -31,6 +33,12 @@ public class EventController {
         if(errors.hasErrors()){
             return ResponseEntity.badRequest().build();
         }
+
+        eventValidator.validate(eventDto, errors);
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
         //eventDto는 id값등이 없기때문에 test에서 입력했어도 값이 들어오지않음
         //modelMapper는 eventDto.setId(...)처럼 하나하나 넣어서 event로 만드는작업을 줄이고 한번에 변환해줌
         Event event = modelMapper.map(eventDto, Event.class);
--- src/test/java/com/example/demo1/events/EventControllerTests.java
+++ src/test/java/com/example/demo1/events/EventControllerTests.java
@@ -101,4 +101,26 @@ public class EventControllerTests {
         ;
     }
 
+    @Test
+    public void createEvent_Bad_Request_Wrong_Input() throws Exception {
+        EventDto eventDto = EventDto.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2021,8,12,20,23))
+                .closeEnrollmentDateTime(LocalDateTime.of(2021,8,11,20,30))
+                .beginEventDateTime(LocalDateTime.of(2020,8,24,20,23))
+                .endEventDateTime(LocalDateTime.of(2020,8,23,20,30))
+                .basePrice(10000)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("하대원동 131")
+                .build();
+        this.mockMvc.perform(post("/api/events")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(this.objectMapper.writeValueAsString(eventDto))
+        )
+                .andExpect(status().isBadRequest())
+        ;
+    }
+
 }
