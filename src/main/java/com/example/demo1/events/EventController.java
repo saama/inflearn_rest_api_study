794afe4281dd113f11c9061bf0755aa7eb1eae33
saama <hjpw237@naver.com>
2021-08-17 21:35:01 +0900

    hateos

--- src/main/java/com/example/demo1/events/EventController.java
+++ src/main/java/com/example/demo1/events/EventController.java
@@ -2,6 +2,7 @@ package com.example.demo1.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -42,8 +43,13 @@ public class EventController {
         //eventDto는 id값등이 없기때문에 test에서 입력했어도 값이 들어오지않음
         //modelMapper는 eventDto.setId(...)처럼 하나하나 넣어서 event로 만드는작업을 줄이고 한번에 변환해줌
         Event event = modelMapper.map(eventDto, Event.class);
+        event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createUri).body(event);
+        ControllerLinkBuilder slash = linkTo(EventController.class).slash(newEvent.getId());
+        URI createUri = slash.toUri();
+        EventResource t = new EventResource(event);
+        t.add(linkTo(EventController.class).withRel("query-events"));
+        t.add(slash.withRel("update-event"));
+        return ResponseEntity.created(createUri).body(t);
     }
 }
